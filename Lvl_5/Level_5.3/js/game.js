d2b2b5b94442f769f0bd91ba60ec58feea381d2b
Price <neprice@email.neit.edu>
2022-05-11 11:20:07 -0400

    start of game

--- Lvl_5/Level_5.3/js/game.js
+++ Lvl_5/Level_5.3/js/game.js
@@ -2,7 +2,6 @@
 	//---------------------In this assignment you will draw multiple squares on the screen in random locations--------------
 	//---------------------First you will create an array of particles. Then you will move and draw them.-------------------
 	//---------------------Follow the comments below to complete this assignment--------------------------------------------
-
 var canvas;
 var context;
 var timer;
@@ -10,14 +9,43 @@ var interval;
 var player;
 
 
+
+function randNum(maxLimit = canvas.width && canvas.height - 1)
+{
+	let rand = Math.random() * maxLimit;
+	//console. log(rand);
+	rand = Math. floor(rand);
+	return rand;
+}
+
+
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");		
 	
 	//----------------------------------------------Step 1: Create particles------------------------------------------------
 	//Declare an "amount" variable and set it to 12
-	
+	var amount = 12;
+	var particals = [];
+	var angle = 90;
+	var y = canvas.height - 200;
+	var vy = 2;
+	var vx = 1;
 	//Create an array called "particles" to store the particles
+
+	for(i = 0; i < amount; i++)
+	{
+		particals[i] = new GameObject();
+		particals[i].color = "red"
+		angle += 90;
+		rad = angle * Math.PI/180;
+		particals[i].x = randNum();
+		particals[i].y = randNum();
+		y += -100;
+		particals[i].vx = 1;
+		particals[i].vy = 2;
+	}
 	
+	console.log(particals);
 	//Create a for loop that loops the number of times specified by the "amount" variable
 
 		//Within the for loop do the following: 
@@ -41,9 +69,33 @@ function animate()
 	context.clearRect(0,0,canvas.width, canvas.height);	
 	
 	
+		
 	//--------------------------------------Step 2: Draw Particles---------------------------------------------------------
 	//DrawRect()for each particle using a for loop.
 	//The for loop should use the particles.length for its limit
+	for(i = 0; i < particals.length; i++)
+	{	
+		if(particals[i].y > canvas.height - particals[i].height/2)
+		{
+			particals[i].vy += -vy;
+		}
+		if(particals[i].y < 0 + particals[i].height/2)
+		{
+			particals[i].vy += -vy;
+		}
+		if(particals[i].x > canvas.width - particals[i].width/2)
+		{
+			particals[i].vx += -vx;
+
+		}
+		if(particals[i].x < 0 + particals[i].width/2 )
+		{
+			particals[i].vx += -vx;
+				
+		}
+		particals[i].drawRect();
+		particals[i].move();
+	}
 	
 	
 	//----------------------------------------------------------------------------------------------------------------------
